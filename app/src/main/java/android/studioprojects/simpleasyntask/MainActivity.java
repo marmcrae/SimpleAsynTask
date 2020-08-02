27a428159b2c47f59e678c127aacd4296a7a4453
marmcrae <mcrae.marquie@gmail.com>
2020-08-01 17:43:16 -1000

    Initial commit

---
+++ app/src/main/java/android/studioprojects/simpleasyntask/MainActivity.java
@@ -0,0 +1,40 @@
+package android.studioprojects.simpleasyntask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TEXT_STATE = "currentText";
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView1);
+
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
+    }
+
+
+    public void startTask(View view) {
+
+        mTextView.setText(R.string.napping);
+
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
+    }
+}
